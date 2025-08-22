06ba704a915070eb759529daca967fc53d65271c
Max <oscar.torres.m@uni.pe>
2025-08-22 17:18:21 -0500

    camera animation modified, needs a game orchestrator (game manager)

--- Assets/_Project/Scripts/Managers/CameraSystem/CameraSystem.cs
+++ Assets/_Project/Scripts/Managers/CameraSystem/CameraSystem.cs
@@ -1,10 +1,14 @@
 using System.Collections;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class CameraSystem : StaticInstance<CameraSystem> {
 	
 	private Transform mainCamera;
-	[SerializeField] private float _speed = 1f;
+	[SerializeField] private float _displacementAngle = 30f;
+	[SerializeField] private float _displacementSpeed = 0.5f;
+	[SerializeField] private float _rotationSpeed = 0.25f;
 	private bool isRotating = false;
 
 	protected override void Awake() {
@@ -14,42 +18,38 @@ public class CameraSystem : StaticInstance<CameraSystem> {
 
 	public void RotateRight() {
 		if(isRotating) return;
-        StartCoroutine(PerformRotation(
-            Quaternion.Euler(0, 0, mainCamera.rotation.eulerAngles.z - 90),
-            _speed
-        ));
-    }
+
+		PerformRotation(_displacementAngle, _displacementSpeed, () => {
+			PerformRotation( -1 * (_displacementAngle + 90), _rotationSpeed);
+		});
+	}
 
 	public void RotateLeft() {
 		if(isRotating) return;
-        StartCoroutine(PerformRotation(
-            Quaternion.Euler(0, 0, mainCamera.rotation.eulerAngles.z + 90),
-            _speed
-        ));
-    }
+
+		PerformRotation( -1 * _displacementAngle, _displacementSpeed, () => {
+			PerformRotation(_displacementAngle + 90, _rotationSpeed);
+		});
+	}
 
 	public void Rotate180() {
 		if(isRotating) return;
-        StartCoroutine(PerformRotation(
-			Quaternion.Euler(0, 0, mainCamera.rotation.eulerAngles.z + 180),
-			_speed
-		));
+		PerformRotation( -1 * _displacementAngle, _displacementSpeed, () => {
+			PerformRotation(_displacementAngle + 180, _rotationSpeed);
+		});
 	}
 
-	private IEnumerator PerformRotation (Quaternion targetRotation,float _speed) {
+	private void PerformRotation(float displacement, float performTime, UnityAction OnComplete = null) {
 		isRotating = true;
-		float progress = 0f;
-		float speed = _speed;
+		mainCamera.DORotate(
+			new Vector3(0, 0, displacement), 
+			performTime, 
+			RotateMode.LocalAxisAdd)
 
-		while (progress < 1f) {
-			mainCamera.rotation = Quaternion.Slerp (mainCamera.rotation, targetRotation, progress);
-			progress += Time.deltaTime * speed;
-
-			if (progress <= 1f) {
-				yield return null;
-			}
-		}
-		isRotating = false;
+		.onComplete = () => { 
+			isRotating = false;
+			OnComplete?.Invoke(); 
+		};
 	}
 
 }
