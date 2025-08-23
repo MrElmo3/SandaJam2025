111600dbc0d33a88e6980e01a79172c9da0de70f
Max <oscar.torres.m@uni.pe>
2025-08-22 19:04:49 -0500

    modified movement controller (remove test methods)

--- Assets/_Project/Scripts/Player/MovementController.cs
+++ Assets/_Project/Scripts/Player/MovementController.cs
@@ -11,10 +11,6 @@ public class MovementController : MonoBehaviour {
 		rb = GetComponent<Rigidbody2D>();
 	}
 
-	private void Start() {
-		StartCoroutine(TestCorrutine());
-	}
-
 	public void OnMove(InputValue value){
 		if (Mathf.Abs(value.Get<float>()) <= 0.01f) return;
 		moveDirection = new(value.Get<float>(), 0);
@@ -25,13 +21,5 @@ public class MovementController : MonoBehaviour {
 		//rb.linearVelocity = moveDirection;
 		//TODO: create a class for the objects affected by this gravity
 		rb.AddForce(GravitySystem.Instance.GetCurrentGravity());
-	}
-
-	private IEnumerator TestCorrutine() {
-		while(true) {
-			yield return new WaitForSeconds(5);
-			GravitySystem.Instance.RotateRight();
-			CameraSystem.Instance.RotateLeft();
-		}
 	}
 }
